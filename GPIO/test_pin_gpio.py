6371b429dcd24a33bf9a40c31460ccc46ab71b09
BarckCode <barckcode@gmail.com>
2020-11-28 15:45:43 +0100

    feat: Add test_pin_gpio.py

---
+++ GPIO/test_pin_gpio.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+
+def main():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(2, GPIO.OUT)
+
+    validation = True
+    while validation:
+        data = input("Indica una orden de control: ")
+        if data == "a":
+            GPIO.output(2, GPIO.HIGH)
+        elif data == "b":
+            GPIO.output(2, GPIO.LOW)
+        elif data == "z":
+            validation = False
+
+
+if __name__ == '__main__':
+    print("Inicio del programa")
+    main()
+    print("Fin del programa")
